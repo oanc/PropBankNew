0baa8a1a7f582ed30df40c467f536e12152b2b3c
smiller <smiller@f9835c53-d872-4803-a61e-d6c59fa17448>
2013-07-16 20:41:34 +0000

    User input functionality added to main class -- inputs are ptbPath propPath inputFile outputPath, in that order.

--- PropBankNew/src/main/java/org/anc/propbank/Constants.java
+++ PropBankNew/src/main/java/org/anc/propbank/Constants.java
@@ -6,10 +6,10 @@ import java.io.IOException;
 
 public class Constants extends org.anc.constants.Constants 
 {
-	@Default("/var/corpora/MASC-3.0.0")
+	@Default("MASC-3.0.0")
 	public final String MASC_ROOT = null;
 	
-	@Default("/var/corpora/MASC-3.0.0/resource-header.xml")
+	@Default("MASC-3.0.0/MASC_3.0.0-resource-header.xml")
 	public final String MASC_RESOURCE_HEADER = null;
 	
 	@Default("test-files")
--- PropBankNew/src/test/java/org/anc/propbank/New_PropbankDriver.java
+++ PropBankNew/src/test/java/org/anc/propbank/New_PropbankDriver.java
@@ -2,8 +2,10 @@ package org.anc.propbank;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.Writer;
+import java.util.Properties;
 
 import org.xces.graf.api.GrafException;
 import org.xces.graf.api.IGraph;
@@ -18,6 +20,21 @@ public class New_PropbankDriver {
 	
 	public static void main(String[] args) throws FileNotFoundException, IOException, SAXException, GrafException
 	{
+		//Define constants based on user input
+		String ptbPath = args[0];
+		String propPath = args[1];
+		String inputFile = args[2];
+		String outputPath = args[3];
+		
+		//Replace constants data in the properties file based on that user input
+		Properties prop = new Properties();
+		prop.setProperty("PTB_DATA_PATH", ptbPath);
+		prop.setProperty("PROP_DATA_PATH", propPath);
+		prop.setProperty("INPUT_FILE", inputFile);
+		prop.setProperty("OUTPUT_DATA_PATH", outputPath);
+		prop.store(new FileOutputStream("conf/sandramiller/org.anc.propbank.Constants.properties"), null);
+		
+		
 		//Initialize path to propbank file
 		File testDir = new File(K.PROP_DATA_PATH);		
 		File testFile = new File(testDir, K.INPUT_FILE + ".prop");
@@ -40,8 +57,7 @@ public class New_PropbankDriver {
 		File dotFile = new File(K.OUTPUT_DATA_PATH + "/" + K.INPUT_FILE + "-pb.dot");
 		DotRenderer dotRenderer = new DotRenderer(dotFile);
 		dotRenderer.render(newGraph);
-		
-		
+
 		// For testing and clarification purposes, initialize a New_PTB_Navigator and demonstrate the navigation function
 		New_PTBNavigator navigator = new New_PTBNavigator(K.PTB_DATA_PATH + "/" + K.INPUT_FILE);
 		
