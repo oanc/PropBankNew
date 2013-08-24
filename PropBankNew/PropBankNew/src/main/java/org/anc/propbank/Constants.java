2c4b313c5ed83e7fa9313b58e29ac27d2bdfee68
smiller <smiller@f9835c53-d872-4803-a61e-d6c59fa17448>
2013-08-24 14:39:13 +0000

    Updated.

--- PropBankNew/PropBankNew/src/main/java/org/anc/propbank/Constants.java
+++ PropBankNew/PropBankNew/src/main/java/org/anc/propbank/Constants.java
@@ -12,16 +12,19 @@ public class Constants extends org.anc.constants.Constants
 	@Default("MASC-3.0.0/MASC_3.0.0-resource-header.xml")
 	public final String MASC_RESOURCE_HEADER = null;
 	
-	@Default("test-files")
+	@Default("new-test-files")
 	public final String PROP_DATA_PATH = null;
 	
-	@Default("test-files")
+	@Default("TXTFILES")
+	public final String TXT_DATA_PATH = null;
+	
+	@Default("new-test-files")
 	public final String PTB_DATA_PATH = null;
 	
 	@Default("output-files")
 	public final String OUTPUT_DATA_PATH = null;
 	
-	@Default("110CYL068")
+	@Default("113CWL017")
 	public final String INPUT_FILE = null;
 	
 	public Constants()
